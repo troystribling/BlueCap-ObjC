32988797b8eb20738566d069e4c6361c040e354c
Troy Stribling <troy.stribling@gmail.com>
2013-09-28 17:20:07 -0700

    added sensor tag services

--- Bluecap/PeripheralDefinitions/TISensorTagPeripheralDefinition.m
+++ Bluecap/PeripheralDefinitions/TISensorTagPeripheralDefinition.m
@@ -14,11 +14,41 @@
 + (void)create {
     [[BlueCapCentralManager sharedInstance] createPeripheralWithName:@"TI BLE Sensor Tag" andDefinition:^(BlueCapPeripheralDefinition* peripheralDefinition) {
         
+        [peripheralDefinition createServiceWithUUID:@"F000AA10-0451-4000-B000-000000000000"
+                                               name:@"Accelerometer"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+
+        [peripheralDefinition createServiceWithUUID:@"F000AA30-0451-4000-B000-000000000000"
+                                               name:@"Magnetometer"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+        
+        [peripheralDefinition createServiceWithUUID:@"F000AA50-0451-4000-B000-000000000000"
+                                               name:@"Gyroscope"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+
         [peripheralDefinition createServiceWithUUID:@"F000AA00-0451-4000-B000-000000000000"
                                                name:@"IR Temperature Sensor"
                                       andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
-        }];
+                                      }];
         
+        [peripheralDefinition createServiceWithUUID:@"F000AA40-0451-4000-B000-000000000000"
+                                               name:@"Barometer"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+
+        [peripheralDefinition createServiceWithUUID:@"F000AA20-0451-4000-B000-000000000000"
+                                               name:@"Humidity Sensor"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+
+        [peripheralDefinition createServiceWithUUID:@"F000AA20-0451-4000-B000-000000000000"
+                                               name:@"Humidity Sensor"
+                                      andDefinition:^(BlueCapServiceDefinition* serviceDefinition) {
+                                      }];
+
     }];
 }
 
