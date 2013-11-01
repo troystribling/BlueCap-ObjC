c4b13c30fea41ac59dd01fa22ba3986005b672d1
Troy Stribling <troy.stribling@gmail.com>
2013-10-31 19:18:21 -0700

    added tabbar images and perheral managr services controller

---
+++ UI/BlueCapUI/BlueCapUI/PeripheralManagerServicesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeripheralManagerServicesViewController.h
+//  BlueCapUI
+//
+//  Created by Troy Stribling on 10/31/13.
+//  Copyright (c) 2013 gnos.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeripheralManagerServicesViewController : UITableViewController
+
+@end
