140c95a38d7e87832cf47c4fed22e76b8ec0176a
Troy Stribling <troy.stribling@gmail.com>
2014-03-27 08:37:30 -0700

    added hex string to data category

---
+++ Bluecap/NSString+DataFromHexString.h
@@ -0,0 +1,15 @@
+//
+//  NSString+DataFromHexString.h
+//  BlueCap
+//
+//  Created by Troy Stribling on 3/27/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (DataFromHexString)
+
+- (NSData*)dataValueFromHexString;
+
+@end
---
+++ Bluecap/NSString+DataFromHexString.m
@@ -0,0 +1,28 @@
+//
+//  NSString+DataFromHexString.m
+//  BlueCap
+//
+//  Created by Troy Stribling on 3/27/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import "NSString+DataFromHexString.h"
+
+@implementation NSString (DataFromHexString)
+
+- (NSData*)dataValueFromHexString {
+    [self stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSMutableData* dataValue = [[NSMutableData alloc] init];
+    unsigned char byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    int i;
+    for (i=0; i < [self length]/2; i++) {
+        byte_chars[0] = [self characterAtIndex:i*2];
+        byte_chars[1] = [self characterAtIndex:i*2+1];
+        byte = strtol(byte_chars, NULL, 16);
+        [dataValue appendBytes:&byte length:1];
+    }
+    return dataValue;
+}
+
+@end
